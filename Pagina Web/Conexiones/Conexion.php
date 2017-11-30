f1c866417430c9b7bd2423dfc438ae8580d0b0a3
diarsaaa <32171293+diarsaaa@users.noreply.github.com>
2017-11-30 10:54:26 -0500

    Update Conexion.php

--- Pagina Web/Conexiones/Conexion.php
+++ Pagina Web/Conexiones/Conexion.php
@@ -1,6 +1,6 @@
 <?php
 //$conexion = mysqli_connect("127.0.0.1","root","sagitario20") or die("<h2>No se encuentra el servidor</h2>");
 //$db = mysqli_select_db($conexion,"pagina_web") or die("<h2>Error de Conexion</h2>");
-$conexion = mysqli_connect("db581164667.db.1and1.com","dbo581164667","serie4000") or die("<h2>No se encuentra el servidor</h2>");
-$db = mysqli_select_db($conexion,"db581164667") or die("<h2>Error de Conexion</h2>");
+$conexion = mysqli_connect("","","") or die("<h2>No se encuentra el servidor</h2>");
+$db = mysqli_select_db($conexion,"") or die("<h2>Error de Conexion</h2>");
 ?>
